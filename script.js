68a1ff3aea69caea1564f6780cde471b9d89d4db
Andreas Berge <Andreas Berge>
2023-02-06 18:43:27 +0100

    fixed glitch in js code

--- script.js
+++ script.js
@@ -3,49 +3,46 @@ const outputText = document.querySelector("#outputText");
 const counter = document.querySelector("span");
 const range = document.querySelector("input");
 
-
 function sectionMaker(text) {
-    const secLength = range.value;
-    console.log(secLength)
-    const charList = [".", "!", "?"];
-    let output = "";
-    let charCount = 0;
-    let charCountDisplay = 0;
-    let paraCountDisplay = 1;
-    for (let char of text) {
-        if (char !== " ") {
-            charCount++;
-            charCountDisplay++;
-        }
-        if (charCount >= secLength && charList.includes(char)) {
-            paraCountDisplay++;
-            output = output + char + "<br><br>";
-            // outputText.textContent = output;
-            charCount = 0;
-        } else output = output + char;
+  const secLength = range.value;
+  console.log(secLength);
+  const charList = [".", "!", "?"];
+  let output = "";
+  let charCount = 0;
+  let charCountDisplay = 0;
+  let secCountDisplay = 1;
+  for (let char of text) {
+    if (char !== " ") {
+      charCount++;
+      charCountDisplay++;
     }
-    counter.textContent = `${charCountDisplay} characters in ${paraCountDisplay} section(s)`;
-    return outputText.innerHTML = output;
+    if (charCount >= secLength && charList.includes(char)) {
+      secCountDisplay++;
+      output = output + char + "<br><br>";
+      charCount = 0;
+    } else output = output + char;
+  }
+  counter.textContent = `${charCountDisplay} characters in ${secCountDisplay} section(s)`;
+  return (outputText.innerHTML = output);
 }
 
 function display() {
-    if (!outputText.textContent) {
-        counter.textContent = "";
-    }
-    if (inputText.value) {
-        sectionMaker(inputText.value);
-        // inputText.value = "";
-    }
+  if (!outputText.textContent) {
+    counter.textContent = "";
+  }
+  if (inputText.value) {
+    sectionMaker(inputText.value);
+  }
 }
 
 document.querySelector("#refreshButton").addEventListener("click", () => {
-    window.location.reload();
-})
+  window.location.reload();
+});
 
 document.querySelector("#submitButton").addEventListener("click", () => {
-    display();
-})
+  display();
+});
 
 range.addEventListener("change", () => {
-    display();
-})
+  if (outputText.innerHTML.trim()) display();
+});
